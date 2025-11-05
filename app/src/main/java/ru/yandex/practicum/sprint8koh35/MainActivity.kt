b6533421e7aad92b40c703a4df12292f992b8009
Boris Deniseno <boris.denisenko@mapbox.com>
2025-11-05 17:54:08 +0100

    Initial commit

---
+++ app/src/main/java/ru/yandex/practicum/sprint8koh35/MainActivity.kt
@@ -0,0 +1,28 @@
+package ru.yandex.practicum.sprint8koh35
+
+import android.os.Bundle
+import android.util.Log
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        Log.d("MainActivity","onCreate")
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_main)
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+    }
+
+    override fun onResume() {
+        super.onResume()
+        Log.d("MainActivity","onResume")
+    }
+}
