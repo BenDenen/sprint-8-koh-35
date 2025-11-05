aff7a1d9fb7b898a2ba59ce54d8115f28d3a19ec
Boris Deniseno <boris.denisenko@mapbox.com>
2025-11-05 19:24:25 +0100

    tasks

--- app/src/main/java/ru/yandex/practicum/sprint8koh35/MainActivity.kt
+++ app/src/main/java/ru/yandex/practicum/sprint8koh35/MainActivity.kt
@@ -1,7 +1,10 @@
 package ru.yandex.practicum.sprint8koh35
 
+import android.content.Intent
 import android.os.Bundle
 import android.util.Log
+import android.widget.Button
+import androidx.activity.OnBackPressedCallback
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
@@ -10,7 +13,7 @@ import androidx.core.view.WindowInsetsCompat
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
-        Log.d("MainActivity","onCreate")
+        Log.d("MainActivity","onCreate $this")
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContentView(R.layout.activity_main)
@@ -19,10 +22,48 @@ class MainActivity : AppCompatActivity() {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
+        findViewById<Button>(R.id.button).apply {
+            setOnClickListener {
+                val intent = Intent(this@MainActivity, MainActivity::class.java)
+                this@MainActivity.startActivity(intent)
+            }
+        }
+
+        onBackPressedDispatcher.addCallback(object : OnBackPressedCallback(true) {
+            override fun handleOnBackPressed() {
+                TODO("Not yet implemented")
+            }
+
+        } )
+    }
+
+    override fun onStart() {
+        super.onStart()
+        Log.d("MainActivity","onStart $this")
     }
 
     override fun onResume() {
         super.onResume()
-        Log.d("MainActivity","onResume")
+        Log.d("MainActivity","onResume $this")
+    }
+
+    override fun onPause() {
+        super.onPause()
+        Log.d("MainActivity","onPause $this")
+    }
+
+    override fun onRestart() {
+        super.onRestart()
+        Log.d("MainActivity","onRestart $this")
+    }
+
+    override fun onStop() {
+        super.onStop()
+        Log.d("MainActivity","onStop $this")
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        Log.d("MainActivity","onDestroy $this")
     }
 }
